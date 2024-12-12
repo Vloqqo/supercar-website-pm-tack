e31ecd6a6bdf3f8405651962b101ca100ba55142
Mr. Fast <33694976+HerrSchnell@users.noreply.github.com>
2024-12-12 13:22:21 -0800

    Created script to show/hide content panels

---
+++ fast.js
@@ -0,0 +1,18 @@
+// Get all the menu-option links
+const menuOptionLinks = document.querySelectorAll('.menu-option');
+
+// Get all the menu-content elements
+const menuContent = document.querySelectorAll('.menu-content');
+
+// Add click event listeners to each menu-option link
+menuOptionLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+        // Hide all menu-content elements
+        menuContent.forEach(content => {
+            content.classList.remove('active');
+        });
+
+        // Show the corresponding menu-content element based on the index
+        menuContent[index].classList.add('active');
+    });
+});
