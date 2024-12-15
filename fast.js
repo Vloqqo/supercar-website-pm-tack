a7d7f944414b20cd6d0b06044d2bf5319b2571ac
Vloqqo <vloqqo@gmail.com>
2024-12-15 05:52:35 -0800

    Added animation

--- fast.js
+++ fast.js
@@ -16,3 +16,28 @@ menuOptionLinks.forEach((link, index) => {
         menuContent[index].classList.add('active');
     });
 });
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+      }
+    }
+  }
+}
+
+var loadingScreen = document.querySelector(".loadingscreen");
+window.addEventListener('load', function() {
+    loadingScreen.style.display = 'none';
+  })
