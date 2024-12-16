e22e109c879af92941e4ee7d59b2c99c9962546b
Vloqqo <vloqqo@gmail.com>
2024-12-15 17:38:46 -0800

    trying to fix stuff

--- fast.js
+++ fast.js
@@ -15,27 +15,7 @@ menuOptionLinks.forEach((link, index) => {
         // Show the corresponding menu-content element based on the index
         menuContent[index].classList.add('active');
     });
-});
-
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width  + "%";
-      }
-    }
-  }
-}
+  });
 
 var loadingScreen = document.querySelector(".loadingscreen");
 window.addEventListener('load', function() {
